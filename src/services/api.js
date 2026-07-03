05e40dbb70b37e61fe0635dff2792eaa6c2132c8
vajrabilashdegapogu <vajrabilashd@gmail.com>
2026-07-03 21:45:31 +0530

    Small web page changes01

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://api.owntag.in/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://api.owntag.in/api",
+  // baseURL: "http://localhost:8080/api",
   timeout: 15000,
   headers: {
     "Content-Type": "application/json"
