33ba4ba0c1b62ed10da043d77ad8f6618a469bd2
owntagqr <owntag.in@gmail.com>
2026-07-20 03:03:10 +0530

    Pre print tags added

--- src/services/api.js
+++ src/services/api.js
@@ -15,7 +15,7 @@
 import axios from "axios";
 
 // export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://api.owntag.in/api";
+export const BASE_URL = "https://api.owntag.in";
 
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
